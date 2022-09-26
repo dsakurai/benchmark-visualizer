668fc15851eacfa7c6fb060ab51d48d1b1ce8568
Sonoda Umi <det_conan@live.cn>
2022-09-26 16:40:51 +0900

    enroute developing alg

--- custom_benchmark_problems/diamon_problem/core/algs.py
+++ custom_benchmark_problems/diamon_problem/core/algs.py
@@ -1,13 +1,26 @@
 from custom_benchmark_problems.diamon_problem.data_structures.tree import Tree
 from pathlib import Path
 
+
 def compute_path(tree_data: dict):
+    info_list = []
     for node in tree_data["nodes"]:
         print(node)
-    pass
+        print(node["symbol"][-1])
+        info_list.append(node["symbol"])
+        print("**")
+    info_list = sorted(info_list)
+    max_len = len(info_list[-1])
+    links = []
+    for sym in info_list:
+        sym.insert(0, 0)
+        if sym not in links:
+            links.append(sym)
+    print(links)
+    max_len -= 1
 
 
-def compute_node_tree():
+def compare_sequence(links, symbol):
     pass
 
 
@@ -15,9 +28,8 @@ if __name__ == "__main__":
     tree = Tree(dim_space=2)
     base_path = Path(__file__).parent.absolute().parents[2]
     data_path = base_path / "sample.json"
-    print(data_path)
     tree.from_json(data_path)
     print(tree)
-    tree.to_json()
-    sequence = tree.read_path(7)
-    tree.evaluate(sequence,[1.0,2.0])
+    compute_path(tree.to_json())
+    sequence = tree.read_path(2)
+    tree.evaluate(sequence, [1.0, 2.0])
