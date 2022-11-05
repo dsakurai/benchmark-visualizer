a76c6b06fc078f0c0ed334f9ab522df572b90952
Likun Liu <det_conan@live.cn>
2022-11-05 12:40:55 +0900

    updating evaluation funciton

--- custom_benchmark_problems/diamon_problem/core/evaluation.py
+++ custom_benchmark_problems/diamon_problem/core/evaluation.py
@@ -9,17 +9,28 @@ def __delta_t(t: float, tau: int) -> float:
     return min(t - tau, 1)
 
 
-def __check_s(sequence: tuple, tau: int) -> bool:
-    return True if len(sequence) == tau else False
+def __s_subset(sequence_info: tuple, t) -> tuple:
+    assert t <= __max_s_length(sequence_info) + 1, "t has exceeded maxmium value"
+    yield sequence_info
+
+
+def __max_s_length(sequence_info: tuple) -> int:
+    return len(sequence_info)
 
 
 def evaluate(
     solution_variables: tuple, sequence_info: tuple, candidate_coordinates: list
 ) -> np.ndarray:
     t = solution_variables[-1]
-    tau = __tau(t)
-    while __check_s(sequence=sequence_info, tau=tau):
+    x = solution_variables[:-1]
+    max_s_length = __max_s_length(sequence_info)
+    if t > max_s_length + 1:
+        tau = max_s_length
+        # TODO: compute f(tau,x)
+    else:
+        # TODO: compute f(t,x)
         pass
+    return np.array([])
 
     return np.array([])
 
