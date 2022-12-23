dcc879feeebdc680a01af99a90212129b88cd5de
Likun Liu <det_conan@live.cn>
2022-12-23 14:18:27 +0900

    possibly add some todos

--- utils/mlflow_tracking.py
+++ utils/mlflow_tracking.py
@@ -12,6 +12,25 @@ import os
 
 
 class Tracking:
+    #
+    # TODO: INTEGRATE THIS FOR TRACK SERVER CHECKING, URGENT
+    # if tracking is True:  # Check if the server is available
+    #     class BadCode(Exception):
+    #         def __init__(self, message):
+    #             BadCode(message)
+    #
+    #     timeout = 5
+    #     try:
+    #         import urllib.request
+    #         code = urllib.request.urlopen(mlflow_url, timeout=timeout).getcode()
+    #         if code != 200: raise BadCode("Website returned code {}".format(code))
+    #     except BadCode:
+    #         raise
+    #     except:
+    #         print(
+    #             "Error: mlflow server error (with {} sec timeout). First, make sure you have access to {} by opening the URL with a browser.".format(
+    #                 timeout, mlflow_url))
+    #         raise
     def __init__(
         self,
         experiment_name: str,
@@ -23,7 +42,7 @@ class Tracking:
         os.environ["MLFLOW_TRACKING_INSECURE_TLS"] = "true"
         urllib3.disable_warnings(InsecureRequestWarning)
         self.experiment_name = experiment_name
-        mlflow.set_tracking_uri(tracking_uri)
+        # mlflow.set_tracking_uri(tracking_uri)
         mlflow.set_experiment(experiment_name)
         if tracking_parameters:
             mlflow.start_run(
@@ -63,6 +82,7 @@ class Tracking:
         self.dict_keys = variable_header + objective_header
 
     def send_data(self):
+        # TODO: Use CSV format
         self.step_metrics = np.array(self.step_metrics)
         file_name = f"{self.experiment_name}_{datetime.datetime.now().isoformat().replace(':','-')}.npz"
         self.step_metrics.dump(file_name)
