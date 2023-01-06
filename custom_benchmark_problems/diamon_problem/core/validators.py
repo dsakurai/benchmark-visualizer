ead1171c757067a11c849fb67adc24878c103443
Likun Liu <det_conan@live.cn>
2023-01-06 23:11:05 +0900

    formatting

--- custom_benchmark_problems/diamon_problem/core/validators.py
+++ custom_benchmark_problems/diamon_problem/core/validators.py
@@ -1,12 +1,13 @@
 from custom_benchmark_problems.diamon_problem.core.evaluation import BMP
 
+
 def validate_tree_minima(sequence_data: list, dim_space: int):
     print("Validating ...")
     sorted_sequence = parse_node_info(sequence_data)
-    t=15
+    t = 15
     while sorted_sequence:
         current_node = sorted_sequence.pop()
-        bmp = BMP(sequence_info=sorted_sequence,dim_space=dim_space)
+        bmp = BMP(sequence_info=sorted_sequence, dim_space=dim_space)
         minimal_coordinates = bmp.compute_coordinates(current_node["attrs"]["symbol"])
         computed_minimal = bmp.evaluate()
 
@@ -19,7 +20,6 @@ def get_minimal_coordinates():
     pass
 
 
-def parse_node_info(sequence_data:list) -> list:
+def parse_node_info(sequence_data: list) -> list:
     sequence_data = sorted(sequence_data, key=lambda node: len(node["attrs"]["symbol"]))
     return sequence_data
-
