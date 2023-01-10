a3d2f31fbfc105f06ce3d650f18a71c59500659c
Likun Liu <det_conan@live.cn>
2023-01-10 21:06:52 +0900

    Add node id into play

--- custom_benchmark_problems/diamon_problem/core/evaluation.py
+++ custom_benchmark_problems/diamon_problem/core/evaluation.py
@@ -34,13 +34,14 @@ class BMP:
         """
         x = solution_variables[1:]
         t = solution_variables[0]
-        y = self.f_t_x(
+        y, node_id = self.f_t_x(
             t=t,
             x=x,
             sequences=self.process_sequence(sequence_info=self.sequence_info),
         )
         if self.rotate:
             t, y = np.matmul(np.array([t, y]), self.rotation_matrix)
+        print(node_id)
         return t, y
 
     def h_x(
@@ -220,9 +221,9 @@ class BMP:
                 s_list.append(element)
         return s_list
 
-    def f_t_x(self, t: int, x: np.ndarray, sequences: dict) -> float:
+    def f_t_x(self, t: int, x: np.ndarray, sequences: dict) -> tuple:
         if t == 0:
-            return 0.0
+            return 0.0,0
         else:
             tau = self.get_tau(t)
             while tau >= 0:
@@ -231,8 +232,8 @@ class BMP:
                 else:
                     f_tau_x = self.f_t_x(t=tau, x=x, sequences=sequences)
                     sequences_tau = sequences[tau]
-                    g_s_values = [f_tau_x]
-                    node_ids = [-999]
+                    g_s_values = [f_tau_x[0]]
+                    node_ids = [f_tau_x[1]]
                     for s_tau in sequences_tau:
                         m_s = s_tau.minima
                         delta_t = t - tau
@@ -242,18 +243,17 @@ class BMP:
                         x_s = self.compute_coordinates(symbol_sequence=s_tau.symbol)
                         M_s = (1.0 - delta_t) * self.f_t_x(
                             tau, x_s, sequences
-                        ) + delta_t * m_s
+                        )[0] + delta_t * m_s
                         delta_x = x - x_s
                         h_x_ = self.h_x(x=x, x_s=x_s, tau=tau)
-                        nabla_g = (self.f_t_x(tau, x_s + h_x_, sequences) - m_s) / h_x_
+                        nabla_g = (self.f_t_x(tau, x_s + h_x_, sequences)[0] - m_s) / h_x_
                         g_s_values.append(M_s + np.dot(nabla_g, delta_x.T))
                         node_ids.append(s_tau.name)
                     # self.f_t_x_[(t, tuple(x.tolist()))] = min(candidates)
                     g_s_values = np.array(g_s_values)
-                    min_index = np. argmax(g_s_values)
+                    min_index = np. argmin(g_s_values)
                     minimal_value = g_s_values[min_index]
-                    print(node_ids[min_index])
-                    return minimal_value
+                    return minimal_value,node_ids[min_index]
 
 
 if __name__ == "__main__":
