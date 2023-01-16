45e40433b6fbf65ef61dd5c41dfec09f20b9a0d4
Likun Liu <det_conan@live.cn>
2023-01-16 19:38:55 +0900

    In the middle of updating solver status update

--- front-end/src/components/NaiveLogs.vue
+++ front-end/src/components/NaiveLogs.vue
@@ -2,14 +2,14 @@
   <el-container>
     <el-header>
       <el-card>
-        <el-slider v-model="currentStep" :step=1 :marks="marks" show-input :show-input-controls="false" :format-tooltip="formatTooltip"/>
+        <el-slider v-model="currentStep" :step="stepSize" :marks="marks" :format-tooltip="formatTooltip"/>
       </el-card>
     </el-header>
     <el-main>
       <el-tree-v2 :data="treeData" :props="props"  :height="208">
         <template #default="{ node }">
           <span>{{ node.label }}</span>
-            <el-icon ><Location /></el-icon>
+            <el-icon ><Location /></el-icon><span v-if="node.value!==solver_data[currentStep / stepSize].eval_node_id"> !!{{solver_data[currentStep / stepSize]}} </span>
            </template>
       </el-tree-v2>
     </el-main>
@@ -25,11 +25,11 @@ export default {
       logData:'',
       treeData:'',
       currentStep:0,
+      totalSteps:100,
+      stepSize:1,
       allIDs:[],
-      marks:{
-        0:"0",
-        100:"25000",
-      },
+      solver_data:{},
+      marks:{},
       props : {
         value: 'id',
         label: 'label',
@@ -43,12 +43,19 @@ export default {
   },
   methods:{
     formatTooltip(val){
-      return val * 250;
+      return val * this.totalSteps / 100;
     },
     getNaiveLogData(){
       axios.get("/api/demo_data").then(response => {
         this.logData = response.data.solver_log;
         this.treeData = response.data.tree;
+        this.totalSteps = this.logData.length;
+        this.stepSize = 100 / this.totalSteps;
+        this.marks[0] = "0";
+        this.marks[100] = this.totalSteps.toString();
+        for (let i = 0;i<this.totalSteps;i++){
+          this.solver_data[this.logData[i].step] = this.logData[i];
+        }
       })
     }
   },
