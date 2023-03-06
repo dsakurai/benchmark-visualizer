34ce61663118b6c91b6290a7bfc9a74bec397178
Likun Liu <det_conan@live.cn>
2023-03-06 17:35:17 +0900

    Update experiment settings

--- yaml_main.py
+++ yaml_main.py
@@ -1,5 +1,6 @@
 import argparse
 import math
+from enum import Enum
 from pathlib import Path
 
 from jmetal.algorithm.multiobjective.gde3 import GDE3
@@ -10,6 +11,85 @@ from jmetal.operator import SBXCrossover, PolynomialMutation
 
 from custom_benchmark_problems.diamon_problem.apis.jmetal import Diamond
 from custom_benchmark_problems.diamon_problem.data_structures.tree import Tree
+
+import typing
+from typing import NamedTuple, Dict, Any
+import yaml
+
+
+class ExperimentSettings(NamedTuple):
+    experiment_name: str
+    tree_file: str
+    dimension: int
+    algorithm: str
+    algorithm_parameters: Dict[str, Any]
+    termination_criterion: Dict[str, Any]
+
+
+class Algorithms(Enum):
+    gde3 = "GDE3"
+    nsgaii = "NSGAII"
+    moead = "MOEAD"
+    omopso = "OMOPSO"
+
+
+def gde3(**kwargs):
+    parameters = kwargs["parameters"]
+    population_size = parameters["population_size"]
+    cr = parameters["cr"]
+    f = parameters["f"]
+
+    return GDE3(problem=kwargs["problem"],
+                population_size=100,
+                cr=0.5,
+                f=0.5,
+                termination_criterion=StoppingByEvaluations(max_evaluations),
+                )
+
+
+def nsgaii(**kwargs):
+    print("nsgaii")
+    pass
+
+
+def moead(**kwargs):
+    pass
+
+
+def omopso(**kwargs):
+    pass
+
+
+def load_experiment_settings(file_path: Path) -> typing.List[ExperimentSettings]:
+    with file_path.open() as file:
+        settings = yaml.safe_load(file)
+    return [ExperimentSettings(**setting) for setting in settings]
+
+
+def yaml_main(opts):
+    exps_config = load_experiment_settings(file_path=Path(opts.file))
+    tracking_parameters = {
+        "run_id": None,
+        "experiment_id": None,
+        "run_name": "First Run",
+        "tags": {"test_version": "0.0.1"},
+        "description": "Tracking parameters are not required, but is a Nice-to-have."
+                       " You could fill some of these parameters for identification",
+    }
+    for exp_config in exps_config:
+        tree = Tree(dim_space=exp_config.dimension)
+        tree.from_json(exp_config.tree_file)
+        problem = Diamond(
+            dim_space=exp_config.dimension,
+            sequence_info=tree.to_sequence(),
+            enable_tracking=opts.disable_tracking,
+            tracking_uri="http://xomics.cc.kyushu-u.ac.jp:5000",
+            experiment_name="test_runx",
+            tracking_parameters=tracking_parameters,
+        )
+        algorithm = globals()[Algorithms(exp_config.algorithm).name](problem=problem,
+                                                                     parameters=exp_config.algorithm_parameters,
+                                                                     termination_criterion=exp_config.termination_criterion)
 
 
 def cli_main(opts):
@@ -24,7 +104,7 @@ def cli_main(opts):
         "run_name": "First Run",
         "tags": {"test_version": "0.0.1"},
         "description": "Tracking parameters are not required, but is a Nice-to-have."
-        " You could fill some of these parameters for identification",
+                       " You could fill some of these parameters for identification",
     }
 
     # Problem construction, again, tracking_parameters are not required
@@ -67,7 +147,7 @@ def cli_main(opts):
     for solution in solutions:
         x_1 = solution.variables[0]
         x_2 = solution.variables[1]
-        y = abs(1 - math.sqrt(x_1**2 + x_2**2) / math.pi)
+        y = abs(1 - math.sqrt(x_1 ** 2 + x_2 ** 2) / math.pi)
         y = -abs(math.sin(x_1) * math.cos(x_2) * math.exp(y))
         if y - solution.objectives[0] > 0:
             print(solution)
@@ -86,5 +166,4 @@ if __name__ == "__main__":
     )
     parser.add_argument("--dim", type=int, help="Dimension of the problem", default=2)
     parser.add_argument("--disable_tracking", action="store_false")
-    cli_main(parser.parse_args())
-    print(time.time()-start_time)
+    yaml_main(parser.parse_args())
