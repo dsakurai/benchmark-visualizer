dc9b5cd4ca53d2cd2e8a1510bed46a4052bd4d69
Likun Liu <det_conan@live.cn>
2023-03-07 16:03:43 +0900

    update alg settings

--- yaml_main.py
+++ yaml_main.py
@@ -1,20 +1,27 @@
 import argparse
 import math
+import typing
 from enum import Enum
 from pathlib import Path
+from typing import NamedTuple, Dict, Any
 
+import yaml
+from jmetal.algorithm.multiobjective import NSGAII, MOEAD, OMOPSO
 from jmetal.algorithm.multiobjective.gde3 import GDE3
-from jmetal.algorithm.multiobjective.nsgaii import NSGAII
+from jmetal.operator import (
+    PolynomialMutation,
+    SBXCrossover,
+    DifferentialEvolutionCrossover,
+    UniformMutation,
+)
+from jmetal.operator.mutation import NonUniformMutation
+from jmetal.util.aggregative_function import Tschebycheff
+from jmetal.util.archive import CrowdingDistanceArchive
 from jmetal.util.solution import get_non_dominated_solutions
-from jmetal.util.termination_criterion import StoppingByEvaluations
-from jmetal.operator import SBXCrossover, PolynomialMutation
+from jmetal.util.termination_criterion import StoppingByTime
 
 from custom_benchmark_problems.diamon_problem.apis.jmetal import Diamond
 from custom_benchmark_problems.diamon_problem.data_structures.tree import Tree
-
-import typing
-from typing import NamedTuple, Dict, Any
-import yaml
 
 
 class ExperimentSettings(NamedTuple):
@@ -38,26 +45,140 @@ def gde3(**kwargs):
     population_size = parameters["population_size"]
     cr = parameters["cr"]
     f = parameters["f"]
+    termination_criterion = StoppingByTime(
+        kwargs["termination_criterion"]["termination_parameter"]
+    )
 
-    return GDE3(problem=kwargs["problem"],
-                population_size=100,
-                cr=0.5,
-                f=0.5,
-                termination_criterion=StoppingByEvaluations(max_evaluations),
-                )
+    return GDE3(
+        problem=kwargs["problem"],
+        population_size=population_size,
+        cr=cr,
+        f=f,
+        termination_criterion=termination_criterion,
+    )
 
 
 def nsgaii(**kwargs):
-    print("nsgaii")
-    pass
+    parameters = kwargs["parameters"]
+    population_size = parameters["population_size"]
+    offspring_population_size = parameters["offspring_population_size"]
+    mutation_parameters = parameters["mutation"]
+    crossover_parameters = parameters["crossover"]
+    if mutation_parameters["probability"] == "n_variables":
+        probability = 1 / kwargs["exp_config"].dimension
+    elif type(mutation_parameters["probability"]) is float:
+        probability = mutation_parameters["probability"]
+    else:
+        raise NotImplementedError("Invalid mutation probability")
+    mutation = PolynomialMutation(
+        probability=probability,
+        distribution_index=mutation_parameters["distribution_index"],
+    )
+    crossover = SBXCrossover(
+        probability=crossover_parameters["probability"],
+        distribution_index=crossover_parameters["distribution_index"],
+    )
+    termination_criterion = StoppingByTime(
+        kwargs["termination_criterion"]["termination_parameter"]
+    )
+
+    return NSGAII(
+        problem=kwargs["problem"],
+        population_size=population_size,
+        offspring_population_size=offspring_population_size,
+        mutation=mutation,
+        crossover=crossover,
+        termination_criterion=termination_criterion,
+    )
 
 
 def moead(**kwargs):
-    pass
+    parameters = kwargs["parameters"]
+    population_size = parameters["population_size"]
+    mutation_parameters = parameters["mutation"]
+    crossover_parameters = parameters["crossover"]
+    if mutation_parameters["probability"] == "n_variables":
+        probability = 1 / (kwargs["exp_config"].dimension + 1)
+    elif type(mutation_parameters["probability"]) is float:
+        probability = mutation_parameters["probability"]
+    else:
+        raise NotImplementedError("Invalid mutation probability")
+    mutation = PolynomialMutation(
+        probability=probability,
+        distribution_index=mutation_parameters["distribution_index"],
+    )
+    crossover = DifferentialEvolutionCrossover(
+        CR=crossover_parameters["CR"],
+        F=crossover_parameters["F"],
+        K=crossover_parameters["K"],
+    )
+    aggregative_function = Tschebycheff(dimension=kwargs["exp_config"].dimension + 1)
+    termination_criterion = StoppingByTime(
+        kwargs["termination_criterion"]["termination_parameter"]
+    )
+
+    return MOEAD(
+        problem=kwargs["problem"],
+        population_size=population_size,
+        mutation=mutation,
+        crossover=crossover,
+        aggregative_function=aggregative_function,
+        neighbor_size=parameters["neighbor_size"],
+        neighbourhood_selection_probability=parameters[
+            "neighbourhood_selection_probability"
+        ],
+        max_number_of_replaced_solutions=parameters["max_number_of_replaced_solutions"],
+        weight_files_path=parameters["weight_files_path"],
+        termination_criterion=termination_criterion,
+    )
 
 
 def omopso(**kwargs):
-    pass
+    parameters = kwargs["parameters"]
+    swarm_size = parameters["swarm_size"]
+    epsilon = parameters["epsilon"]
+
+    uniform_mutation_parameters = parameters["uniform_mutation"]
+    if uniform_mutation_parameters["probability"] == "n_variables":
+        uniform_probability = 1 / (kwargs["exp_config"].dimension + 1)
+    elif type(uniform_mutation_parameters["probability"]) is float:
+        uniform_probability = uniform_mutation_parameters["probability"]
+    else:
+        raise NotImplementedError("Invalid mutation probability")
+    uniform_mutation = UniformMutation(
+        probability=uniform_probability,
+        perturbation=uniform_mutation_parameters["perturbation"],
+    )
+
+    non_uniform_mutation_parameters = parameters["non_uniform_mutation"]
+    if uniform_mutation_parameters["probability"] == "n_variables":
+        non_uniform_probability = 1 / (kwargs["exp_config"].dimension + 1)
+    elif type(uniform_mutation_parameters["probability"]) is float:
+        non_uniform_probability = non_uniform_mutation_parameters["probability"]
+    else:
+        raise NotImplementedError("Invalid mutation probability")
+    non_uniform_mutation = NonUniformMutation(
+        probability=non_uniform_probability,
+        perturbation=non_uniform_mutation_parameters["perturbation"],
+        max_iterations=int(25000 / 100),
+    )
+
+    leaders = CrowdingDistanceArchive(
+        maximum_size=parameters["leaders"]["maximum_size"]
+    )
+    termination_criterion = StoppingByTime(
+        kwargs["termination_criterion"]["termination_parameter"]
+    )
+
+    return OMOPSO(
+        problem=kwargs["problem"],
+        swarm_size=swarm_size,
+        epsilon=epsilon,
+        uniform_mutation=uniform_mutation,
+        non_uniform_mutation=non_uniform_mutation,
+        leaders=leaders,
+        termination_criterion=termination_criterion,
+    )
 
 
 def load_experiment_settings(file_path: Path) -> typing.List[ExperimentSettings]:
@@ -74,7 +195,7 @@ def yaml_main(opts):
         "run_name": "First Run",
         "tags": {"test_version": "0.0.1"},
         "description": "Tracking parameters are not required, but is a Nice-to-have."
-                       " You could fill some of these parameters for identification",
+        " You could fill some of these parameters for identification",
     }
     for exp_config in exps_config:
         tree = Tree(dim_space=exp_config.dimension)
@@ -87,76 +208,31 @@ def yaml_main(opts):
             experiment_name="test_runx",
             tracking_parameters=tracking_parameters,
         )
-        algorithm = globals()[Algorithms(exp_config.algorithm).name](problem=problem,
-                                                                     parameters=exp_config.algorithm_parameters,
-                                                                     termination_criterion=exp_config.termination_criterion)
+        algorithm = globals()[Algorithms(exp_config.algorithm).name](
+            problem=problem,
+            exp_config=exp_config,
+            parameters=exp_config.algorithm_parameters,
+            termination_criterion=exp_config.termination_criterion,
+        )
+        algorithm.run()
 
+        # Get solutions
+        solutions = algorithm.get_result()
 
-def cli_main(opts):
-    # Construct the tree from file
-    tree = Tree(dim_space=opts.dim)
+        # Print the results
+        for solution in solutions:
+            x_1 = solution.variables[0]
+            x_2 = solution.variables[1]
+            y = abs(1 - math.sqrt(x_1**2 + x_2**2) / math.pi)
+            y = -abs(math.sin(x_1) * math.cos(x_2) * math.exp(y))
+            if y - solution.objectives[0] > 0:
+                print(solution)
 
-    tree.from_json(opts.file)
+        print("**********************")
 
-    tracking_parameters = {
-        "run_id": None,
-        "experiment_id": None,
-        "run_name": "First Run",
-        "tags": {"test_version": "0.0.1"},
-        "description": "Tracking parameters are not required, but is a Nice-to-have."
-                       " You could fill some of these parameters for identification",
-    }
-
-    # Problem construction, again, tracking_parameters are not required
-    problem = Diamond(
-        dim_space=opts.dim,
-        sequence_info=tree.to_sequence(),
-        enable_tracking=opts.disable_tracking,
-        tracking_uri="http://xomics.cc.kyushu-u.ac.jp:5000",
-        experiment_name="test_runx",
-        tracking_parameters=tracking_parameters,
-    )
-
-    # GDE3 Settings
-    max_evaluations = 25000
-
-    algorithm = GDE3(
-        problem=problem,
-        population_size=100,
-        cr=0.5,
-        f=0.5,
-        termination_criterion=StoppingByEvaluations(max_evaluations),
-    )
-
-    algorithm = NSGAII(
-        problem=problem,
-        population_size=100,
-        offspring_population_size=100,
-        mutation=PolynomialMutation(probability=1.0 / problem.number_of_variables, distribution_index=20),
-        crossover=SBXCrossover(probability=1.0, distribution_index=20),
-        termination_criterion=StoppingByEvaluations(max_evaluations),
-    )
-
-    # Run GDE3
-    algorithm.run()
-
-    # Get solutions
-    solutions = algorithm.get_result()
-
-    # Print the results
-    for solution in solutions:
-        x_1 = solution.variables[0]
-        x_2 = solution.variables[1]
-        y = abs(1 - math.sqrt(x_1 ** 2 + x_2 ** 2) / math.pi)
-        y = -abs(math.sin(x_1) * math.cos(x_2) * math.exp(y))
-        if y - solution.objectives[0] > 0:
-            print(solution)
-
-    print("**********************")
-
-    fronts = get_non_dominated_solutions(solutions)
-    for front in fronts:
-        print(front)
+        fronts = get_non_dominated_solutions(solutions)
+        for front in fronts:
+            print(front)
 
 
 if __name__ == "__main__":
