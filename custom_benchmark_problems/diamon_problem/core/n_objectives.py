1edfe3c88bb581460fffaf402407d9b8efd8151b
Likun Liu <det_conan@live.cn>
2024-03-18 17:29:05 +0900

    Implementing nd rotation matrix

---
+++ custom_benchmark_problems/diamon_problem/core/n_objectives.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from custom_benchmark_problems.diamon_problem.core.evaluation import BMP, EvaluationResult
+
+
+class NBMP:
+    def __init__(self, sequence_info: list[dict], dim_space: int,n_objectives:int, rotate: bool = True, degrees: int = 45, clockwise: bool = True):
+        self.bmp = BMP(sequence_info=sequence_info, dim_space=dim_space, rotate=False)
+        self.n_objectives = n_objectives
+        self.rotate = rotate
+        # Define rotate matrix information here
+        theta = np.radians(degrees)
+        c, s = np.cos(theta), np.sin(theta)
+
+
+    def evaluate(self, solution_variables: np.ndarray) -> EvaluationResult:
+        np.linalg.multi_dot()
+        pass
+
+    def generate_rotation_matrix(self, dimension_index:int):
+        base_matrix = np.identity(self.n_objectives)
+
+    def parse_variables(self, solution_variables: np.ndarray,dimension: int) -> np.ndarray:
