f89057a4d8d9d0c45803b16ba3cc2ea31b8a9a06
Likun Liu <det_conan@live.cn>
2024-09-14 19:41:22 +0900

    Updating Pareto analysis code

--- utils/reference_fronts.py
+++ utils/reference_fronts.py
@@ -9,19 +9,19 @@ from custom_benchmark_problems.diamon_problem.data_structures.tree import Tree
 
 
 class ReferenceFronts:
-    """ This class is for compute the actual Pareto front (in theory)
+    """This class is for compute the actual Pareto front (in theory)"""
 
-    """
     def __init__(self):
         pass
 
     def sampling_domain_axis(self):
         pass
 
-
-    def get_local_pareto_set(self, dimension: int, tree_name: str):
+    def get_local_pareto_set(
+        self, dimension: int, tree_name: str, resolution: int = 100
+    ):
         tree = Tree(dim_space=dimension)
-        tree.from_json(f"experiment_trees/{tree_name}.json")
+        tree.from_json(f"../experiment_trees/{tree_name}.json")
         sequence_info = tree.to_sequence()
         bmp = evaluation.BMP(sequence_info=sequence_info, dim_space=dimension)
         pareto_dict = {}
@@ -56,9 +56,9 @@ class ReferenceFronts:
             else:
                 appearing_time = minimal_time - 1
 
-            t = np.linspace(appearing_time, bmp.t_upper_bound(), 100)
+            t = np.linspace(appearing_time, bmp.t_upper_bound(), resolution)
             central_coordinates = np.broadcast_to(
-                central_coordinates, (100, len(central_coordinates))
+                central_coordinates, (resolution, len(central_coordinates))
             )
             pareto_set = np.insert(central_coordinates, 0, t, axis=1)
             pareto_front = np.apply_along_axis(apply_computing, axis=1, arr=pareto_set)
@@ -77,3 +77,9 @@ class ReferenceFronts:
         # for node_id in pareto_dict.keys():
         #     pareto_set = pareto_dict[node_id]["pareto_set"]
         #     pareto_front = pareto_dict[node_id]["pareto_front"]
+
+
+if __name__ == "__main__":
+    rf = ReferenceFronts()
+    res = rf.get_local_pareto_set(dimension=2, tree_name="breadth", resolution=10)
+    print(res[0])
