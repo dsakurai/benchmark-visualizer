87dd4aa777aca314704fae8c2ffa3315db09190e
Likun Liu <liu.likun.654@s.kyushu-u.ac.jp>
2024-10-21 10:04:58 +0900

    Experiments analytics

--- run_300_times.sh
+++ run_300_times.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the number of times you want to run the command
-NUM_TIMES=300
+NUM_TIMES=6
 
 # Define the command you want to run
 COMMAND="echo 'Hello, World!'"
@@ -10,5 +10,5 @@ COMMAND="echo 'Hello, World!'"
 for ((i=1; i<=NUM_TIMES; i++))
 do
     echo "Running iteration $i"
-    python yaml_main_parallel.py -f exp_config_N-obj-test.yaml --n_objective
+    python yaml_main_parallel.py -f exp_config_N-obj-test-MOEAD.yaml --n_objective
 done
